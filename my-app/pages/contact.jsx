022d459e2e74be61069a104aac8bede055f11b80
karengomezs <karengomezsoto1@gmail.com>
2022-12-04 16:05:17 -0500

    form contact structure ok

--- my-app/pages/contact.jsx
+++ my-app/pages/contact.jsx
@@ -4,7 +4,27 @@ export default function Contact() {
   return (
     <>
       <NavBar />
-      <h1>hoivn</h1>
+      <h2>Want to know more?</h2>
+      <p>Send us your questions and we will contact you</p>
+
+      <form>
+        <div>
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input type="text" className="form-control" id="name" />
+        </div>
+        <div>
+          <label htmlFor="Email1" className="form-label">
+            Email address
+          </label>
+          <input type="email" className="form-control" id="Email1" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Send
+        </button>
+      </form>
     </>
   );
 }
